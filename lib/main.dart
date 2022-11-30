3b07cb09d41108ed5e197ffa3c36db26b6cce295
mott4vitor <dailyslokoscontatoofc@gmail.com>
2022-11-30 10:28:51 -0300

    task: anotações na main.dart + stores de exemplo do Contador

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:pomodoro/pages/pomodoro.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
+import './store/pomodoro.store.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,17 +13,32 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Pomodoro',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    //==================================================================================================
+    // * Então, para consumir nossa PomodoroStore, precisamos importar o provider(nesse caso)
+    // onde iremos utilizar. Aqui, no main.dart, vamos usar o MultiProvider para criar nosso "provedor".
+    //
+    // * Esse MultiProvider acompanha de um providers que recebe um array [] e em seguida,
+    // colocamos nossa STORE dentro dele e passando via Generics<>.
+    // Lembrando que nessa array, podemos colocar mais de uma STORE.
+    //
+    //  * Segue abaixo:
+    return MultiProvider(
+      providers: [
+        Provider<PomodoroStore>(
+          create: (_) => PomodoroStore(), // método que vai devolver a nossa STORE
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Pomodoro',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: PomodoroPage(),
       ),
-      home: PomodoroPage(),
     );
   }
 }
-
 
 // ============================================================================================== //
 // import 'package:flutter_mobx/flutter_mobx.dart';
@@ -48,24 +66,24 @@ class MyApp extends StatelessWidget {
 //               'You have pushed the button this many times:',
 //             ),
 
-            //================================================================================
-            // Adiciono um Observer para colocar a minha ação aqui dentro, com sua reatividade
-            //================================================================================
-      //       Observer(
-      //         builder: ((_) => Text(
-      //               '${store.contador}',
-      //               style: Theme.of(context).textTheme.headline4,
-      //             )),
-      //       ),
-      //     ],
-      //   ),
-      // ),
+//================================================================================
+// Adiciono um Observer para colocar a minha ação aqui dentro, com sua reatividade
+//================================================================================
+//       Observer(
+//         builder: ((_) => Text(
+//               '${store.contador}',
+//               style: Theme.of(context).textTheme.headline4,
+//             )),
+//       ),
+//     ],
+//   ),
+// ),
 
-      //=======================================================================================
-      // Então, aqui nesse botão eu passo o incremento criado na Store, que posteriormente ele
-      // vai estar modificando o valor do nosso @observable.
-      // Chamando a seguinde linha: store.incrementar
-      //=======================================================================================
+//=======================================================================================
+// Então, aqui nesse botão eu passo o incremento criado na Store, que posteriormente ele
+// vai estar modificando o valor do nosso @observable.
+// Chamando a seguinde linha: store.incrementar
+//=======================================================================================
 //       floatingActionButton: FloatingActionButton(
 //         onPressed: store.incrementar,
 //         tooltip: 'Incrementar',
