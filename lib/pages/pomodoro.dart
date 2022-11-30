52e0cde07334de9c1bea4af5477b7342da0c25a8
mott4vitor <dailyslokoscontatoofc@gmail.com>
2022-11-30 10:28:12 -0300

    task: implementação de Observer() na página do Pomodoro

--- lib/pages/pomodoro.dart
+++ lib/pages/pomodoro.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:pomodoro/components/cronometro.dart';
 import 'package:pomodoro/components/entrada_tempo.dart';
+import 'package:pomodoro/store/pomodoro.store.dart';
+import 'package:provider/provider.dart';
 
 class PomodoroPage extends StatelessWidget {
   const PomodoroPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final store = Provider.of<PomodoroStore>(context);
+
     return Scaffold(
       body: SafeArea(
         child: Column(
@@ -17,18 +22,24 @@ class PomodoroPage extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 40),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: const [
-                  EntradaTempo(
-                    titulo: 'Trabalho',
-                    valor: 25,
-                  ),
-                  EntradaTempo(
-                    titulo: 'Descanso',
-                    valor: 5,
-                  ),
-                ],
+              child: Observer(
+                builder: (_) => Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    EntradaTempo(
+                      titulo: 'Trabalho',
+                      valor: store.tempoTrabalho,
+                      inc: store.incrementarTempoTrabalho,
+                      dec: store.decrementarTempoTrabalho,
+                    ),
+                    EntradaTempo(
+                      titulo: 'Descanso',
+                      valor: store.tempoDescanso,
+                      inc: store.incrementarTempoDescanso,
+                      dec: store.decrementarTempoDescanso,
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
