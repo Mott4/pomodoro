0aba039e1b2eb550f8ec3980cbb8a55ad4c5963c
mott4vitor <dailyslokoscontatoofc@gmail.com>
2022-12-03 12:54:26 -0300

    task: criação de Timer para o cronometro e algumas lógicas usando if else

--- lib/store/pomodoro.store.dart
+++ lib/store/pomodoro.store.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
+
 import 'package:mobx/mobx.dart';
 
 part 'pomodoro.store.g.dart';
 
 class PomodoroStore = _PomodoroStore with _$PomodoroStore;
+
+enum TipoIntervalo { TRABALHO, DESCANSO }
 
 abstract class _PomodoroStore with Store {
   // =============================================================== //
@@ -10,14 +14,58 @@ abstract class _PomodoroStore with Store {
   // =============================================================== //
 
   @observable
+  bool iniciado = false;
+
+  @observable
+  int minutos = 2;
+
+  @observable
+  int segundos = 0;
+
+  @observable
   int tempoTrabalho = 2;
 
   @observable
   int tempoDescanso = 1;
 
+  @observable
+  TipoIntervalo tipoIntervalo = TipoIntervalo.TRABALHO;
+
   // =============================================================== //
   //     ações para incrementar e decrementar tempo  do pomodoro     //
   // =============================================================== //
+
+  Timer? cronometro;
+
+  // tempo iniciado =====================================================
+  @action
+  void iniciar() {
+    iniciado = true;
+    cronometro = Timer.periodic(Duration(milliseconds: 50), (timer) {
+      if (minutos == 0 && segundos == 0) {
+        _trocarTipoIntervalo();
+      } else if (segundos == 0) {
+        segundos = 59;
+        minutos--;
+      } else {
+        segundos--;
+      }
+    });
+  }
+
+  // tempo parado =======================================================
+  @action
+  void parar() {
+    iniciado = false;
+    cronometro?.cancel();
+  }
+
+  // tempo reiniciar ====================================================
+  @action
+  void reiniciar() {
+    iniciado = false;
+    parar();
+  }
 
   // tempo trabalho ====================================================
   @action
@@ -39,5 +87,26 @@ abstract class _PomodoroStore with Store {
   @action
   void decrementarTempoDescanso() {
     tempoDescanso--;
+  }
+
+  // função bool para enum ===============================================
+
+  bool estaTrabalhando() {
+    return tipoIntervalo == TipoIntervalo.TRABALHO;
+  }
+
+  bool estaDescansando() {
+    return tipoIntervalo == TipoIntervalo.DESCANSO;
+  }
+
+  void _trocarTipoIntervalo() {
+    if (estaTrabalhando()) {
+      tipoIntervalo = TipoIntervalo.DESCANSO;
+      minutos = tempoDescanso;
+    } else {
+      minutos = tempoDescanso;
+      tipoIntervalo = TipoIntervalo.TRABALHO;
+    }
+    segundos = 0;
   }
 }
